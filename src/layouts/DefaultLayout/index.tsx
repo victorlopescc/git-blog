907363f95b8a6e20e03eca5a0eb95b3763d353ac
victorlopescc <victorlopes.cc@gmail.com>
2024-06-14 15:34:58 -0300

    feat: Add Header component with logo and background image

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-// import { Header } from "../../components/Header";
+import { Header } from "../../components/Header";
 import { LayoutContainer } from "./styles";
 
 export function DefaultLayout() {
   return (
     <LayoutContainer>
-      {/* <Header /> */}
+      <Header />
       <main>
         <Outlet />
       </main>
